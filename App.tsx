fe0730e1cd77f44d5064192cdca8ff2614f25b72
JPedroValarini <joaopedrovalarini@gmail.com>
2025-07-15 16:36:42 -0300

    add detail screen

--- App.tsx
+++ App.tsx
@@ -1,15 +1,34 @@
-import { StatusBar, StyleSheet, View } from 'react-native';
+import React from 'react';
+import { StatusBar, StyleSheet } from 'react-native';
 import { ApolloProvider } from '@apollo/client';
 import client from './src/api/apolloClient';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import HomeScreen from './src/screens/HomeScreen';
+import DetailScreen from './src/screens/DetailScreen';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <View style={styles.container}>
+      <NavigationContainer>
         <StatusBar barStyle="dark-content" />
-        <HomeScreen navigation={{ navigate: () => { } }} />
-      </View>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: 'Pokémons' }}
+          />
+          <Stack.Screen
+            name="Details"
+            component={DetailScreen}
+            options={{ title: 'Detalhes do Pokémon' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </ApolloProvider>
   );
 }
@@ -21,3 +40,4 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+export { styles as appStyles };
